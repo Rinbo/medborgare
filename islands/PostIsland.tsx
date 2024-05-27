5e22eafcfb05f70501ba16bd6c2fb74510f34938
Robin Börjesson <robin.b@outlook.com>
2024-05-27 21:15:48 +0200

    Improve add post behaviour

--- islands/PostIsland.tsx
+++ islands/PostIsland.tsx
@@ -1,10 +1,18 @@
 import { useSignal } from "@preact/signals";
-import { Post } from "kv/posts.ts";
+import { Comment, Post } from "kv/posts.ts";
 import AutoSizeTextArea from "islands/forms/AutoSizeTextArea.tsx";
 import { ROUTES } from "route-utils";
+import formatDistanceToNow from "https://deno.land/x/date_fns@v2.22.1/formatDistanceToNow/index.js";
 
 export default function PostIsland({ post, isLoggedIn }: { post: Post; userId: string; isLoggedIn: boolean }) {
-  const errors = useSignal([]);
+  const comments = useSignal(post.comments ?? []);
+
+  function scrollToBottom() {
+    globalThis.scrollTo({
+      top: document.body.scrollHeight,
+      behavior: "smooth",
+    });
+  }
 
   function onSubmit(e: SubmitEvent) {
     e.preventDefault();
@@ -18,31 +26,44 @@ export default function PostIsland({ post, isLoggedIn }: { post: Post; userId: s
       headers: { ContentType: "application/json" },
     })
       .then((res) => res.json())
-      .then((_json) => {
-        window.location.reload();
+      .then((json) => {
         target.reset();
+        comments.value = [...comments.value, json.comment];
+        scrollToBottom();
       })
-      .catch((err) => (errors.value = err?.errors?.text || []));
+      .catch((err) => console.error(err));
   }
 
   return (
-    <div class="flex flex-col gap-2">
+    <div class="mb-2 flex flex-col gap-1">
       <div class="flex h-full flex-col gap-1 rounded-md border p-4">
-        <div class="font-semibold text-primary">{post.userName}</div>
+        <div class="font-semibold text-primary">@{post.userName}</div>
         <div class="text-xs italic">{new Date(post.createdAt).toLocaleDateString()}</div>
         <p class="whitespace-pre-line">{post.body}</p>
       </div>
       {isLoggedIn && <CommentForm onSubmit={onSubmit} />}
-      {post.comments?.map((comment) => <div key={comment.id} class="truncate">{comment.text}</div>)}
+      {comments.value?.map((comment) => <CommentPanel key={comment.id} comment={comment} />)}
+    </div>
+  );
+}
+
+function CommentPanel({ comment }: { comment: Comment }) {
+  return (
+    <div class="flex flex-col gap-1 rounded-md border px-4 py-2">
+      <span class="flex flex-row gap-4">
+        <div class="text-xs font-extralight">@{comment.userName}</div>
+        <div class="text-xs">{formatDistanceToNow(new Date(comment.createdAt), { addSuffix: true })}</div>
+      </span>
+      <div class="whitespace-pre-line">{comment.text}</div>
     </div>
   );
 }
 
 function CommentForm({ onSubmit }: { onSubmit: (e: SubmitEvent) => void }) {
   return (
-    <form onSubmit={onSubmit}>
+    <form class="rounded-md border px-4 py-2" onSubmit={onSubmit}>
       <AutoSizeTextArea name="text" placeholder="Lägg till en kommentar" rows={1} />
-      <button tabindex={0} class="btn btn-primary float-right my-1">Svara</button>
+      <button type="submit" tabindex={0} class="btn btn-primary btn-sm float-right my-1">Svara</button>
     </form>
   );
 }
--- routes/_app.tsx
+++ routes/_app.tsx
@@ -4,7 +4,7 @@ export default function App({ Component }: PageProps) {
     <html data-theme="cupcake">
       <head>
         <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, maximum-scale=1, minimum-scale=1" />
         <title>medborgare</title>
         <link rel="stylesheet" href="/styles.css" />
       </head>
